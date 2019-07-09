f7b8510a3e3a14195de0b3c5115a34f1af92e0ff
Courtney Brousseau <courtney.brousseau@gsa.gov>
2019-07-09 16:20:27 -0400

    Add documentation to GitHub utils

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function formatDate(date) {
  * 
  * @param {String} dateString A date as a string.
  *
- * @return {Boolean} Is dateString in the correct format (YYYY-MM-DD)?
+ * @return {Boolean} is dateString in the correct format (YYYY-MM-DD)?
  */
 function isValidDateString(dateString) {
     return /^\d\d\d\d-\d\d-\d\d$/.test(dateString);
@@ -146,11 +146,128 @@ function isValidDateString(dateString) {
  * 
  * @param {Date} date A Date object.
  *
- * @return {Boolean} Is date a valid Date object?
+ * @return {Boolean} is date a valid Date object?
  */
 function isValidDate(date) {
     return date instanceof Date && !isNaN(date);
 }
+
+// GITHUB UTILS 
+
+/**
+ * Calculates the total star count for a repository.
+ * 
+ * @param {JSON} repo repository data from Github API
+ *
+ * @return {Number} total star count for repo
+ */
+function getStarCount(repo) {
+    return repo.repository.stargazers.totalCount;
+}
+
+/**
+ * Calculates the total watch count for a repository.
+ * 
+ * @param {JSON} repo repository data from Github API
+ *
+ * @return {Number} total watch count for repo
+ */
+function getWatchCount(repo) {
+    return repo.repository.watchers.totalCount;
+}
+
+/**
+ * Calculates the total fork count for a repository.
+ * 
+ * @param {JSON} repo repository data from Github API
+ *
+ * @return {Number} total fork count for repo
+ */
+function getForkCount(repo) {
+    return repo.repository.forks.totalCount;
+}
+
+/**
+ * Calculates the total issue count for a repository.
+ * 
+ * @param {JSON} repo repository data from Github API
+ *
+ * @return {Number} total issue count for repo
+ */
+function getIssueCount(repo) {
+    return repo.repository.issues.totalCount;
+}
+
+/**
+ * Calculates the total pull request count for a repository.
+ * 
+ * @param {JSON} repo repository data from Github API
+ *
+ * @return {Number} total pull request count for repo
+ */
+function getPullRequestCount(repo) {
+    return repo.repository.pullRequests.totalCount;
+}
+
+/**
+ * Determines if an authorAssociation indicates the author is internal
+ * 
+ * In this case, an internal author refers to a member of the 
+ * code.gov team (i.e. an owner, member, or collaborator on
+ * the repository being examined)
+ * 
+ * @param {String} authorAssociation authorAssociation from a 
+ * repository's data from GitHub API
+ *
+ * @return {boolean} is the author internal?
+ */
+function authorIsInternal(authorAssociation) {
+    return authorAssociation === "OWNER" || authorAssociation === "MEMBER" || authorAssociation === "COLLABORATOR"; 
+}
+
+/**
+ * Determines if an authorAssociation indicates the author is external
+ * 
+ * In this case, an external author refers to someone who is not
+ * a member of the code.gov team (i.e. a contributor or no association
+ * to the repository being examined)
+ * 
+ * @param {String} authorAssociation authorAssociation from a 
+ * repository's data from GitHub API
+ *
+ * @return {boolean} is the author external?
+ */
+function authorIsExternal(authorAssociation) {
+    return authorAssociation === "FIRST_TIMER" || authorAssociation === "FIRST_TIME_CONTRIBUTOR" || authorAssociation === "CONTRIBUTOR" || authorAssociation === "NONE"; 
+}
+
+/**
+ * Determines if an authorAssociation indicates the author is a 
+ * first time contributor
+ * 
+ * In this case, a first time contributor refers to someone who is not
+ * a member of the code.gov team who has just made their first contribution
+ * to the repository being examined
+ * 
+ * @param {String} authorAssociation authorAssociation from a 
+ * repository's data from GitHub API
+ *
+ * @return {boolean} is the author a first time contributor?
+ */
+function authorIsFirstTimeContributor(authorAssociation) {
+    return authorAssociation === "FIRST_TIMER" || authorAssociation === "FIRST_TIME_CONTRIBUTOR";
+}
+
+// GENERAL UTILS 
+
+/**
+ * Logs an example of the correct format of command line arguments.
+ */
+function logExampleCommandLineArguments() {
+    console.log("For example: node index.js 2019-12-01 2019-12-31");
+}
+
+// ******************************** MAIN PROCESS ********************************
 
 async function queryGitHub(repoName) {
     const endpoint = 'https://api.github.com/graphql';
@@ -352,38 +469,6 @@ function aggregateRepoData(repos) {
         contributorsThisPeriodFirstTimeContributor: unionSetSize(repos.map(repo => repo.contributorsListThisPeriodFirstTimeContributor))
     };
     return totalData;
-}
-
-function getStarCount(repoData) {
-    return repoData.repository.stargazers.totalCount;
-}
-
-function getWatchCount(repoData) {
-    return repoData.repository.watchers.totalCount;
-}
-
-function getForkCount(repoData) {
-    return repoData.repository.forks.totalCount;
-}
-
-function getIssueCount(repoData) {
-    return repoData.repository.issues.totalCount;
-}
-
-function getPullRequestCount(repoData) {
-    return repoData.repository.pullRequests.totalCount;
-}
-
-function authorIsInternal(authorAssociation) {
-    return authorAssociation === "OWNER" || authorAssociation === "MEMBER" || authorAssociation === "COLLABORATOR"; 
-}
-
-function authorIsExternal(authorAssociation) {
-    return authorAssociation === "FIRST_TIMER" || authorAssociation === "FIRST_TIME_CONTRIBUTOR" || authorAssociation === "CONTRIBUTOR" || authorAssociation === "NONE"; 
-}
-
-function authorIsFirstTimeContributor(authorAssociation) {
-    return authorAssociation === "FIRST_TIMER" || authorAssociation === "FIRST_TIME_CONTRIBUTOR";
 }
 
 function getIssueMetaData(repoData) {
@@ -660,13 +745,6 @@ async function writeCSV(data) {
     });
 
     csvWriter.writeRecords(data).then(() => console.log('The CSV file was written successfully'));
-}
-
-/**
- * Logs an example of the correct format of command line arguments.
- */
-function logExampleCommandLineArguments() {
-    console.log("For example: node index.js 2019-12-01 2019-12-31");
 }
 
 // Create global START_DATE and END_DATE variables to be set in the validateCommandLineArguments function
